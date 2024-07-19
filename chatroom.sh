f83b46c1407aec4a2a3339567965333bc499410d
root <root@ip-72-167-43-38.ip.secureserver.net>
2024-07-19 01:21:47 +0000

    update recent changes

--- chatroom.sh
+++ chatroom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the OpenAI API Key
-export OPENAI_API_KEY="sk-proj-9bzUjfla16LDhaEZQaCNT3BlbkFJ9XdoTew3RkDhtN72ryt1"
+export OPENAI_API_KEY="< your api key>"
 
 # Optional: Print a confirmation message
 echo "OPENAI_API_KEY is set to $OPENAI_API_KEY"
