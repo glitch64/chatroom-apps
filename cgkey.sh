1627053b53cddc37bb81c020a291b2c7c8d4a62d
GA Mackey <glitch64@gmail.com>
2024-07-20 14:41:02 +0000

    Overwriten

--- cgkey.sh
+++
@@ -1,1 +0,0 @@
-echo "export OPENAI_API_KEY='sk-None-o1x9KsurPWIUCK9JYuIST3BlbkFJgd9NLkTob89lq8cTAkR3'"
--- chatroom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the OpenAI API Key
-export OPENAI_API_KEY="sk-None-o1x9KsurPWIUCK9JYuIST3BlbkFJgd9NLkTob89lq8cTAkR3"
-
-# Optional: Print a confirmation message
-echo "OPENAI_API_KEY is set to $OPENAI_API_KEY"
-
-node /home/glitch/code/chatroom-apps/server.mjs
